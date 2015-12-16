3c84f68e8662eb02afcd1bb6c09419c071f3ab48
Maliganis Nicholas <mnikolaos@gmail.com>
2015-12-16 14:21:13 +0200

    2nd Changes

--- src/main/java/com/mibeez/hubBe/controllers/HwController.java
+++ src/main/java/com/mibeez/hubBe/controllers/HwController.java
@@ -6,17 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HwController {
 
     private static GpioPinDigitalOutput pin;
+    private GpioController gpio;
+
+    public HwController(){
+        gpio = GpioFactory.getInstance();
+    }
 
     @RequestMapping("/")
     public String Ack(){
         return "Hub Be is Running!";
     }
 
-    @RequestMapping("/light")
-    public String light(){
+    @RequestMapping("/light1")
+    public String lightGpio01(){
+        if(pin == null) {
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED_01", PinState.LOW);
+        }
+
+        pin.toggle();
+        return pin.getState().toString();
+    }
+
+    @RequestMapping("/light2")
+    public String lightGpio02(){
         if(pin == null) {
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED_02", PinState.LOW);
         }
 
         pin.toggle();
